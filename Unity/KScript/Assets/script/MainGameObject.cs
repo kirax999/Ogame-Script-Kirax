ec0ebaedc23eff8bd9800ed7f68fd59227d75800
Alexandre Richet <alexandre.richet@epitech.eu>
2017-09-24 19:30:32 +0200

    If return add alpha color (0.66)

--- Unity/KScript/Assets/script/MainGameObject.cs
+++ Unity/KScript/Assets/script/MainGameObject.cs
@@ -63,7 +63,9 @@ public class MainGameObject : MonoBehaviour {
 				break;
 			}
 			if (obj.isReturn == "true")
-				color += "00";
+				color += "AA";
+			else 
+				color += "FF";
 
 			foreach (var item in go.GetComponentsInChildren<Text>()) {
 				if (item.name == "destCoords")
@@ -110,6 +112,7 @@ public class MainGameObject : MonoBehaviour {
 		byte r = byte.Parse(hex.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
 		byte g = byte.Parse(hex.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
 		byte b = byte.Parse(hex.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
-		return new Color32(r,g,b, 255);
+		byte a = byte.Parse(hex.Substring(6,2), System.Globalization.NumberStyles.HexNumber);
+		return new Color32(r,g,b, a);
 	}
 }
